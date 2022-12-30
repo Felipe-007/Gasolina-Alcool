a3644ad9141e6f05bd3dbdde963ebc4bb2f4018a
Felipe Lopes <felipe.presidencia@gmail.com>
2022-12-30 17:59:30 -0300

    C1 - Front

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
  * Gasolina ou Álcool
  */
 import React, { Component } from "react";
-import { View, Text, Image, TextInput } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity, Modal, Button } from "react-native";
 import { styles } from "./styles";
 
 export default class App extends Component {
@@ -10,10 +10,28 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      gasolina: '',
+      alcool: '',
+      visivel: false,
     };
+
+    this.enviar = this.enviar.bind(this)
   }
+
+  enviar() {
+    this.setState({ visivel: true })
+    if (this.state.gasolina === '' || this.state.alcool === '') {
+      alert('Preencha todos dados corretamente!')
+      return;
+    }
+  }
+
+  fechar(estatus) {
+    this.setState({ visivel: estatus })
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
         <View style={styles.header}>
@@ -27,8 +45,47 @@ export default class App extends Component {
 
           <TextInput
             style={styles.input}
-            placeholder = 'R$ '
+            placeholder='R$ '
+            keyboardType="numeric"
+            onChangeText={(valor) => this.setState({ alcool: valor })}
           />
+
+          <Text style={styles.texto2}>Gasolina (preço por litro):</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder='R$ '
+            keyboardType="numeric"
+            onChangeText={(valor) => this.setState({ gasolina: valor })}
+          />
+
+          <TouchableOpacity style={styles.botao} onPress={this.enviar}>
+            <Text style={styles.botaoTexto}>Enviar</Text>
+          </TouchableOpacity>
+
+          <Modal animationType="slide" visible={this.state.visivel} transparent={true}>
+            <View style={styles.viewModal1}>
+              <Image
+                source={require('./src/img/gas.png')}
+                style={styles.imgModal}
+              />
+
+              <View style={styles.viewModal2}>
+                <Text style={styles.textoModal1}>Compensa usar Álcool</Text>
+              </View>
+
+              <Text style={styles.textoModal2}>Com os preços:</Text>
+
+              <View style={styles.viewModal3}>
+                <Text style={styles.textoModal3}>Álcool: R$ 4.60</Text>
+                <Text style={styles.textoModal3}>Álcool: R$ 4.60</Text>
+              </View>
+
+              <TouchableOpacity style={styles.botaoVoltar} onPress={() => this.fechar(false)}>
+                <Text style={styles.textoVoltar}>Calcular novamente</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
         </View>
       </View>
     )
--- styles.js
+++ styles.js
@@ -19,19 +19,81 @@ export const styles = StyleSheet.create({
   body: {
     alignItems: 'flex-start',
     flex: 2,
-    padding: 18,
+    paddingLeft: 18,
+    marginTop: 71,
   },
   texto2: {
     fontSize: 18,
-    marginTop: 71,
-    color: '#FFF'
-  },
-  input: {
-    height: 48,
-    width: 180,
-    backgroundColor: '#FFFFFF',
-    fontSize: 20,
-    padding: 10,  // margem inicial
+    color: '#FFF',
     marginTop: 10,
   },
+  input: {
+    backgroundColor: '#FFFFFF',
+    fontSize: 18,
+    height: 48,
+    width: 354,
+    marginBottom: 5,
+    borderRadius: 6,
+    marginTop: 10,
+    paddingLeft: 19,
+  },
+  botao: {
+    height: 48,
+    width: 354,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EF4130',
+    marginTop: 21,
+  },
+  botaoTexto: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    color: '#FFFFFF'
+  },
+  viewModal1: {
+    backgroundColor: '#292929',
+    alignItems: 'center',
+    flex: 1,
+  },
+  imgModal: {
+    marginTop: 90
+  },
+  viewModal2: {
+    padding: 35
+  },
+  textoModal1: {
+    color: '#30EF5A',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  textoModal2: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  viewModal3: {
+    padding: 13,
+  },
+  textoModal3: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    lineHeight: 30,
+  },
+  botaoVoltar: {
+    height: 48,
+    width: 354,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    marginTop: 21,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#EF4130',
+    backgroundColor: 'transparent',
+  },
+  textoVoltar: {
+    fontSize: 20,
+    color: '#EF4130',
+    fontWeight: 'bold',
+  }
 })
